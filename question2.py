2cac745338169520c2e0970c09b2166b71cd1eb1
john4020 <37849157+john4020@users.noreply.github.com>
2018-03-28 19:36:08 -0400

    Final for 2a

--- question2.py
+++ question2.py
@@ -1,5 +1,6 @@
-########################################################
-#2222222222222222222222222222222222222222222222222222222
+
+import pandas
+
 def euclidean_distance(x_data, y_data, x_cluster, y_cluster):
     squared_distance = (x_data - x_cluster)**2 + (y_data - y_cluster)**2
     return squared_distance**.5
@@ -12,29 +13,28 @@ def chebyshev_distance(x_data, y_data, x_cluster, y_cluster):
     y_distance = abs(y_data - y_cluster)
     return max(x_distance,y_distance)
 
-##test stuff
-##x_data = 1
-##y_data = -7
-##x_cluster = 2
-##y_cluster = 7
-##print(chebyshev_distance(x_data,y_data,x_cluster,y_cluster))
-  
-test_data = (3,4)
-x_data = test_data[0]
-y_data = test_data[1]
 
-training_data = [(3,5,1),(4,7,1),(5,8,0),(6,6,0),(4,9,0)]    ###training data entries are (study,sleep,pass/fail)
+
+# Getting the data
+sleep_data_frame = pandas.read_excel("StudySleep.xlsx")
+sleep_data = sleep_data_frame.as_matrix()
+
+training_data = sleep_data[:10]
+test_data = sleep_data[10:12]
 
     
-def k_nn_euclidean(k,test_data,training_data,distance_function):  
+def k_nn_euclidean(k,test_point,training_data,distance_function):  
     
     distance_list = []
     
+    x_test = test_point[0]
+    y_test = test_point[1]
+
     for i in training_data:
         x_training = i[0]
         y_training = i[1]
         label_training = i[2]
-        distance = distance_function(x_data,y_data, x_training, y_training)
+        distance = distance_function(x_test,y_test, x_training, y_training)
         distance_list += [(distance,label_training)]
 
     distance_list.sort()
@@ -44,15 +44,20 @@ def k_nn_euclidean(k,test_data,training_data,distance_function):
     fail_counter = 0
     
     for i in distance_list:
-        if i[1] == 1:
+        if i[1] == "Pass":
             pass_counter += 1
-        if i[1] == 0:
+        if i[1] == "Fail":
             fail_counter += 1
     
     if pass_counter > fail_counter:
-        return 1
+        return "Pass"
     if fail_counter > pass_counter:
-        return 0          
-    
-#####print(k_nn_euclidean(3,test_data,training_data,euclidean_distance))
-########################################################
+        return "Fail"
+
+ 
+# Output of results
+# To change value of k, change the first argument of the k_nn_euclidean function
+print("Based on 3-NN, the prediction for test point 1 is:")
+print(k_nn_euclidean(3,test_data[0],training_data,euclidean_distance))
+print("\nBased on 3-NN, the prediction for test point 2 is:")
+print(k_nn_euclidean(3,test_data[1],training_data,euclidean_distance))
